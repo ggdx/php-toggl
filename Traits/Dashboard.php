f2b94e77e97d5065af4b093b8bca86a497ceb785
Daniel White <daniel@romiko.co.uk>
2016-11-21 12:20:24 +0000

    Dashboard trait

--- Traits/Dashboard.php
+++ Traits/Dashboard.php
@@ -2,5 +2,15 @@
 
 trait Dashboard{
 
-
+    /**
+    * Get Dashboard
+    *
+    *
+    * @param int $w_id Workspace ID. If not set, User default for API key owner used.
+    * @return Object
+    */
+    public function getDashboard($w_id = false)
+    {
+        return !$w_id ? $this->call('get','dashboard') ? $this->call('get','dashboard/'.$w_id);
+    }
 }
