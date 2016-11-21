8010be991d786b9e746576ea2ffe5d7b4f89aa92
Daniel White <daniel@romiko.co.uk>
2016-11-21 21:55:42 +0000

    Workspace User trait

--- Traits/WorkspaceUsers.php
+++ Traits/WorkspaceUsers.php
@@ -3,4 +3,60 @@
 trait WorkspaceUsers{
 
 
+    /**
+    * Invite users to workspace
+    *
+    *
+    * @param int $id Workspace ID
+    * @param array $email Invitee email addresses ['example@whatevs.com','whatevs@example.com']
+    * @return object
+    */
+    public function inviteUsers($id = false, array $email = [])
+    {
+        if(!$id){
+             $this->set_error_id(__FUNCTION__);
+        }
+
+        if(!count($email)){
+             $this->set_error(__FUNCTION__.'() - $email array requires at least one email.');
+        }
+
+        $data = new stdClass();
+        $data->emails = $email;
+
+        return $this->call('get', 'workspaces/'.$id.'/invite', $data);
+    }
+
+
+    /**
+    * Get workspace users
+    *
+    *
+    * @param int $id Workspace ID
+    * @return object
+    */
+    public function getWorkspaceUsers($id = false)
+    {
+        if(!$id){
+             $this->set_error_id(__FUNCTION__);
+        }
+        return $this->call('get', 'workspaces/'.$id.'/workspace_users');
+    }
+
+
+    /**
+    * Delete workspace user
+    *
+    *
+    * @param int $id Workspace ID
+    * @return void
+    */
+    public function deleteWorkspaceUser($id = false)
+    {
+        if(!$id){
+             $this->set_error_id(__FUNCTION__);
+        }
+        return $this->call('delete', 'workspace_users/'.$id);
+    }
+
 }
